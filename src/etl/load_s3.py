c5b023fcb8ce8d7b02605fde7caf9cdfbf15aac8
S-Delowar <sayed.buet97@gmail.com>
2025-02-17 10:39:45 +0600

    file uploading configured to s3

--- src/etl/load_s3.py
+++ src/etl/load_s3.py
@@ -1,13 +1,56 @@
+import os
 import pandas as pd
+from dotenv import load_dotenv
 
-from src.etl.transform import get_final_df_on_mongodb_and_postgres_data
+from src.utils.logger import logging
+
+import boto3
+from botocore.exceptions import ClientError
 
 
-final_df = get_final_df_on_mongodb_and_postgres_data()
+load_dotenv()
+
+# Define client for s3
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION"),
+)
+
+
+
+def upload_file_to_s3(local_filepath, bucket_name, s3_key=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if s3_key is None:
+        s3_key = os.path.basename(local_filepath)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(local_filepath, bucket_name, s3_key)
+        logging.info(f"File successfully uploaded from '{local_filepath}' to s3 bucket '{bucket_name}'")
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 
 
 
 
 if __name__=="__main__":
-    print(f"Data is ready to push s3 bucket")
-    print(final_df.info())
+    # Define final data path
+    final_data_path = "Processed_Data/final_combined_data.csv"
+    # get bucket name from environment
+    bucket_name = os.getenv("S3_BUCKET_NAME")
+    # uploading to s3
+    upload_file_to_s3(final_data_path, bucket_name)
