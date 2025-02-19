7cd90e60fb77e9e437758138dd9efa0f35931391
S-Delowar <sayed.buet97@gmail.com>
2025-02-19 08:28:26 +0600

    cicd

--- dags/etl_pipeline.py
+++ dags/etl_pipeline.py
@@ -1,8 +1,11 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.decorators import task
 from datetime import datetime, timedelta
 import sys
 import os
+
+import pandas as pd
 
 # The DAG is at /opt/airflow/airflow/dags/etl_pipeline.py
 # The project root is /opt/airflow
@@ -13,7 +16,7 @@ if project_root not in sys.path:
 from src.etl.extract_mongodb import get_validated_mongodb_data
 from src.etl.extract_postgresql import get_validated_postgres_data
 from src.etl.transform import get_final_combined_data
-from src.etl.load_s3 import initiate_uploading_final_data_to_s3
+from src.etl.load_s3 import upload_dataframe_to_s3
 
 # Define default_args
 default_args = {
@@ -26,39 +29,100 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-# Define DAG
-dag = DAG(
-    "ETL_Pipeline",
+
+
+with DAG(
+    "etl_pipeline_with_tasks",
     default_args=default_args,
-    description="Extract, Transform and Load data to S3",
+    description="ETL pipeline that extracts from MongoDB and PostgreSQL, transforms, and loads to S3 using @task",
     schedule_interval="@daily",
     catchup=False,
-)
+) as dag:
 
-# Define Tasks
-extract_mongo_task = PythonOperator(
-    task_id="extract_mongo",
-    python_callable=get_validated_mongodb_data,
-    dag=dag,
-)
+    @task
+    def extract_mongodb():
+        """
+        Extract validated MongoDB data.
+        Returns the data as a JSON string.
+        """
+        mongo_df = get_validated_mongodb_data()
+        # Convert DataFrame to JSON for XCom passing
+        return mongo_df.to_json()
 
-extract_postgres_task = PythonOperator(
-    task_id="extract_postgres",
-    python_callable=get_validated_postgres_data,
-    dag=dag,
-)
+    @task
+    def extract_postgres():
+        """
+        Extract validated PostgreSQL data.
+        Returns the data as a JSON string.
+        """
+        postgres_df = get_validated_postgres_data()
+        return postgres_df.to_json()
 
-transform_task = PythonOperator(
-    task_id="transform_data",
-    python_callable=get_final_combined_data,
-    dag=dag,
-)
+    @task
+    def transform_data(mongo_json: str, postgres_json: str):
+        """
+        Transform the data by combining MongoDB and PostgreSQL datasets.
+        Returns the final combined data as a JSON string.
+        """
+        # Convert JSON back to DataFrames
+        mongo_df = pd.read_json(mongo_json)
+        postgres_df = pd.read_json(postgres_json)
+        final_df = get_final_combined_data(mongo_df, postgres_df)
+        return final_df.to_json()
 
-load_s3_task = PythonOperator(
-    task_id="load_to_s3",
-    python_callable=initiate_uploading_final_data_to_s3,
-    dag=dag,
-)
+    @task
+    def load_to_s3(final_json: str):
+        """
+        Load the transformed data into an S3 bucket.
+        """
+        bucket_name = os.getenv("S3_BUCKET_NAME")
+        s3_key = "Processed_Data/fuel_consumption_data.csv"
+        final_df = pd.read_json(final_json)
+        upload_dataframe_to_s3(final_df, bucket_name, s3_key)
 
-# Define Dependencies
-[extract_mongo_task, extract_postgres_task] >> transform_task >> load_s3_task
+    # Define task dependencies using the bitshift operators
+    mongo_json = extract_mongodb()
+    postgres_json = extract_postgres()
+    final_json = transform_data(mongo_json, postgres_json)
+    load_to_s3(final_json)
+
+
+
+
+
+# # Define DAG
+# dag = DAG(
+#     "ETL_Pipeline",
+#     default_args=default_args,
+#     description="Extract, Transform and Load data to S3",
+#     schedule_interval="@daily",
+#     catchup=False,
+# )
+
+# # Define Tasks
+# extract_mongo_task = PythonOperator(
+#     task_id="extract_mongo",
+#     python_callable=get_validated_mongodb_data,
+#     dag=dag,
+# )
+
+# extract_postgres_task = PythonOperator(
+#     task_id="extract_postgres",
+#     python_callable=get_validated_postgres_data,
+#     dag=dag,
+# )
+
+# transform_task = PythonOperator(
+#     task_id="transform_data",
+#     python_callable=get_final_combined_data,
+#     dag=dag,
+# )
+
+# load_s3_task = PythonOperator(
+#     task_id="load_to_s3",
+#     python_callable=initiate_uploading_final_data_to_s3,
+#     dag=dag,
+# )
+
+# # Define Dependencies
+# [extract_mongo_task, extract_postgres_task] >> transform_task >> load_s3_task
--- src/etl/load_s3.py
+++ src/etl/load_s3.py
@@ -44,42 +44,6 @@ def upload_dataframe_to_s3(df, bucket_name, s3_key):
 
 
 
-# def upload_file_to_s3(local_filepath, bucket_name, s3_key=None):
-#     """Upload a file to an S3 bucket
-
-#     :param file_name: File to upload
-#     :param bucket: Bucket to upload to
-#     :param object_name: S3 object name. If not specified then file_name is used
-#     :return: True if file was uploaded, else False
-#     """
-
-#     # If S3 object_name was not specified, use file_name
-#     if s3_key is None:
-#         s3_key = os.path.basename(local_filepath)
-
-#     # Upload the file
-#     s3_client = boto3.client('s3')
-#     try:
-#         response = s3_client.upload_file(local_filepath, bucket_name, s3_key)
-#         logging.info(f"File successfully uploaded from '{local_filepath}' to s3 bucket '{bucket_name}'")
-#     except ClientError as e:
-#         logging.error(e)
-#         return False
-#     return True
-
-
-
-# def initiate_uploading_final_data_to_s3():
-#     try:
-#         # Define final data path
-#         final_data_path = "Processed_Data/final_combined_data.csv"
-#         # get bucket name from environment
-#         bucket_name = os.getenv("S3_BUCKET_NAME")
-#         # uploading to s3
-#         upload_file_to_s3(final_data_path, bucket_name)
-#     except Exception as e:
-#         raise CustomException(e, sys)
-
 
 
 
